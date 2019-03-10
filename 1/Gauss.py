4f55ccb91cc154e70fd05c3702aa57925d03b4ae
valerapon <valera_pon@mail.ru>
2019-03-10 21:54:04 +0600

    Gauss upgrade

--- 1/Gauss.py
+++ 1/Gauss.py
@@ -1,27 +1,23 @@
 import numpy as np
-import scipy.linalg as alg
-import matplotlib.pyplot as plt
 
-def func1(A, f, N):
-    for k in range(N):
-        for i in range(k + 1, N):
-            A[k][i] = A[k][i] / A[k][k]
-        f[k] = f[k] / A[k][k]
-        A[k][k] = 1
-        for i in range(k + 1, N):
-            for j in range(k + 1, N):
-                A[i][j] = A[i][j] - A[k][j] * A[i][k]
-            f[i] = f[i] - f[k] * A[i][k]
-            A[i][k] = 0
-    return A, f
-
-def func2(A, f, N):
+def backward(A, f, N):
     x = [0] * N
     for i in range(N - 1, -1, -1):
         x[i] = f[i]
         for j in range(i + 1, N):
             x[i] = x[i] - A[i][j] * x[j]
     return np.array(x)
+
+def forward(A, f, N):
+    for k in range(N):
+        tmp = A[k][k]
+        A[k] = A[k] / tmp
+        f[k] = f[k] / tmp
+        for i in range(k + 1, N):
+            A[i] = A[i] - A[k] * A[i][k]
+            f[i] = f[i] - f[k] * A[i][k]
+            A[i][k] = 0
+    return A, f
 
 N = int(input())
 A = np.random.rand(N, N)
@@ -31,8 +27,6 @@ for i in range(N):
         Sum = Sum + abs(A[i][j])
     A[i][i] = Sum + 1
 f = np.random.rand(N)
-#x = np.linalg.solve(A, f)
-#print(x)
-A, f = func1(A, f, N)
-x = func2(A, f, N)
+A, f = forward(A, f, N)
+x = backward(A, f, N)
 print(x)
