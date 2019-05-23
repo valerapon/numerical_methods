573c16b044abac0d31cbd9fc2478f16b3426c165
Valeriy <39806983+valerapon@users.noreply.github.com>
2019-05-23 18:53:17 +0600

    Add files via upload

--- 4/diffusion.py
+++ 4/diffusion.py
@@ -1,16 +1,17 @@
 import numpy as np
 import pygame
+from pygame import gfxdraw
 
 def f():
-    newMatrix = np.ones((N,N)) * 0.0
+    newMatrix = np.zeros((N,N))
     for i in range(N):
         for j in range(N):
             if i == 0 or i == N - 1 or j == 0 or j == N - 1:
                 newMatrix[i][j] = 0.0
             else:
-                newMatrix[i][j] = matrix[i][j] + mu * tau / (h ** 2) * \
-                                  (matrix[i - 1][j] + matrix[i + 1][j] + matrix[i][j - 1] + matrix[i][j + 1] - 4 * matrix[i][j])
-                if (newMatrix[i][j] < 1):
+                newMatrix[i][j] = matrix[i][j] + C * \
+                                  (matrix[i - 1][j] + matrix[i + 1][j] + matrix[i][j - 1] + matrix[i][j + 1] - 4.0 * matrix[i][j])
+                if (newMatrix[i][j] < 1.0):
                     newMatrix[i][j] = 0.0
     return newMatrix
 
@@ -18,29 +19,21 @@ def draw():
     for i in range(N):
         for j in range(N):
             if matrix[i][j] > 0.0 and matrix[i][j] < 1024.0:
-                pygame.draw.line(window, (255, 255 - int(matrix[i][j]) % 256, 255 - int(matrix[i][j]) % 256), (i, j), (i, j), 1)
+                gfxdraw.pixel(window, i, j, (255, 255 - int(matrix[i][j]) % 256, 255 - int(matrix[i][j]) % 256))
             elif matrix[i][j] > 1024.0:
-                pygame.draw.line(window, (255, 0, 0), (i, j), (i, j), 1)
+                gfxdraw.pixel(window, i, j, (250, 0, 0))
             else:
-                pygame.draw.line(window, (255, 255, 255), (i, j), (i, j), 1)
+                gfxdraw.pixel(window, i, j, (255, 255, 255))
 
-mu = 0.25
-tau = 1.0
-h = 1.0
+C = 0.25
 
 N = 100
-matrix = np.ones((N, N)) * 0.0
+matrix = np.zeros((N, N))
 
-WHITE = (255, 255, 255)
-RED = (225, 0, 50)
-GREEN = (0, 225, 0)
- 
 pygame.init()
 window = pygame.display.set_mode((N, N))
-window.fill(WHITE)
+window.fill((255, 255, 255))
 pygame.display.update()
-
-x, y, t = [], [], []
 
 while True:
     matrix = f()
@@ -50,6 +43,6 @@ while True:
     pressed = pygame.mouse.get_pressed()
     pos = pygame.mouse.get_pos()
     if pressed[0]:
-        matrix[pos[0]][pos[1]] = 255.0
+        matrix[pos[0]][pos[1]] = 400.0
     draw()
     pygame.display.update()
